68ed2ca444df35870540654f1dfeef47c1ba1eb3
SvartMetal <montsevmi@gmail.com>
2014-03-24 21:59:59 +0400

    buffer lengths setted to 1024

--- src/settings.h
+++ src/settings.h
@@ -6,7 +6,7 @@ struct settings {
     static const constexpr char* local_socket_address = "/tmp/simple-telnetd";
     static const size_t server_thread_pool_size = 5;
     static const size_t port = 12345;
-    static const size_t session_buffer_length = 2;
+    static const size_t session_buffer_length = 1024;
     static const size_t process_buffer_length = 1024;
 };
 
